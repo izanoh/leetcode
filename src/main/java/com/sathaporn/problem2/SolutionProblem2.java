f23cf33c5dbc63148c382b5b347b50eeee9b7171
Sathaporn W <sathaporn@orcsoft.co.th>
2023-06-13 20:49:29 +0700

    update problem2: revise code1

--- src/main/java/com/sathaporn/problem2/SolutionProblem2.java
+++ src/main/java/com/sathaporn/problem2/SolutionProblem2.java
@@ -11,54 +11,41 @@ public class SolutionProblem2 {
 		ListNode currentInL2 = l2;
 		
 		int carryPlus = 0;
+		int sumCurrent = 0;
 		
-		int sumCurrent = currentInL1.val + currentInL2.val + carryPlus;
-		if (sumCurrent / 10 > 0) {
-			carryPlus = 1;
-		}
-		currentOutputNode.val = sumCurrent%10;
-		
-		while (hasMoreElements(currentInL1) || hasMoreElements(currentInL2) || carryPlus == 1) {
-			int currentL1 = 0;
-			if (hasMoreElements(currentInL1)) {
+		while (null != currentInL1 || null != currentInL2 || carryPlus == 1) {
+			sumCurrent = 0;
+			if (null != currentInL1) {
+				sumCurrent += currentInL1.val;
 				currentInL1 = currentInL1.next;
-				currentL1 = currentInL1.val;
 			}
-			int currentL2 = 0;
-			if (hasMoreElements(currentInL2)) {
-				currentInL2 = currentInL2.next;
-				currentL2 = currentInL2.val;
-			}
-			sumCurrent = currentL1 + currentL2 + carryPlus;
 			
-			if (sumCurrent / 10 > 0) {
-				carryPlus = 1;
-			} else {
-				carryPlus = 0;
+			if (null != currentInL2) {
+				sumCurrent += currentInL2.val;
+				currentInL2 = currentInL2.next;
 			}
-
-			currentOutputNode.next = new ListNode(sumCurrent%10);
-			currentOutputNode = currentOutputNode.next;
+			
+			sumCurrent += carryPlus;
+			
+			carryPlus = sumCurrent / 10;
+			currentOutputNode.val = sumCurrent%10;
+			
+			if ((null != currentInL1 || null != currentInL2 || carryPlus == 1)) {
+				currentOutputNode.next = new ListNode();
+				currentOutputNode = currentOutputNode.next;
+			}
+			
 		}
 
 		return outputNode;
 	}
-
-	public boolean hasMoreElements(ListNode node) {
-		return null != node.next;
-	}
 	
 	public String convertNodeToString(ListNode listNode) {
-		String strNode1 = "";
-		if (null == listNode) {
-			return "";
-		} else {
-			strNode1 = listNode.val + "";
-		}
-		while (hasMoreElements(listNode)) {
+		String strNode = "";
+		while (null != listNode) {
+			strNode = strNode + listNode.val;
 			listNode = listNode.next;
-			strNode1 = strNode1 + (String.valueOf(listNode.val));
 		}
-		return strNode1;
+		return strNode;
 	}
 }
