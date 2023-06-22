fee6b03dfb07a17a6d9aeab89ef650d52d30e3ef
Sathaporn W <sathaporn@orcsoft.co.th>
2023-06-22 11:25:04 +0700

    update: solution4 reduce memory

--- src/main/java/com/sathaporn/problem4/SolutionProblem4.java
+++ src/main/java/com/sathaporn/problem4/SolutionProblem4.java
@@ -1,51 +1,67 @@
 package com.sathaporn.problem4;
 
-import java.util.Arrays;
-
 public class SolutionProblem4 {
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int lenghtNum1 = nums1.length;
 		int lenghtNum2 = nums2.length;
 		
-		int[] all = new int[lenghtNum1+lenghtNum2];
+		int previous = 0;
+		int current = 0;
 		
 		int position = 0;
 		int indexNum1 = 0, indexNum2 = 0;
+
+		boolean isOddLength = ((lenghtNum1+lenghtNum2) % 2)==1;
+		int medianPosition = (((lenghtNum1+lenghtNum2) / 2));
 		
 		while (indexNum1 < lenghtNum1 && indexNum2 < lenghtNum2) {
 
 			if (nums1[indexNum1] <= nums2[indexNum2]) {
-				all[position] = nums1[indexNum1];
+				current = nums1[indexNum1];
 				indexNum1++;
 			} else {
-				all[position] = nums2[indexNum2];
+				current = nums2[indexNum2];
 				indexNum2++;
-				
 			}
-			System.out.println("indexNum1 : " + indexNum1 + " indexNum2 : " + indexNum2 + " arr : "+Arrays.toString(all));
+			
+			if (position == medianPosition ) {
+				if (isOddLength) {
+					return current/1.0;
+				} else {
+					return (current+ previous )/2.0;
+				}
+			}
+			previous = current;
 			position++;
 		}
 		
 		while (indexNum1 < lenghtNum1) {
-			all[position] = nums1[indexNum1];
+			current = nums1[indexNum1];
+			if (position == medianPosition ) {
+				if (isOddLength) {
+					return current/1.0;
+				} else {
+					return (current+ previous )/2.0;
+				}
+			}
+			previous = current;
 			indexNum1++;
             position++;
         }
 
         while (indexNum2 < lenghtNum2) {
-        	all[position] = nums2[indexNum2];
+        	current = nums2[indexNum2];
+        	if (position == medianPosition ) {
+				if (isOddLength) {
+					return current/1.0;
+				} else {
+					return (current+ previous )/2.0;
+				}
+			}
+			previous = current;
         	indexNum2++;
             position++;
         }
-		System.out.println(Arrays.toString(all));
-		
-		if (all.length % 2 ==0) {
-			int medianPosition = ((all.length / 2))-1;
-
-			return (all[medianPosition] + all[medianPosition+1]) / 2.0;
-		} else {
-			int medianPosition = ((all.length / 2));
-			return all[medianPosition]/1.0;
-		}
+		return current/1.0;
 	}
 }
